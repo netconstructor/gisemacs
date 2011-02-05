49b84e1cbbd74b2a2689d8b1932dd06537cf26f1
unknown <Marko@.(none)>
2011-02-05 22:04:13 +0100

    cedet radi emacsw32

--- gisemacs.el
+++ gisemacs.el
@@ -1,7 +1,9 @@
-(add-to-list 'load-path (expand-file-name "~/gisemacs/"))
+﻿(add-to-list 'load-path (expand-file-name "~/gisemacs/"))
 (add-to-list 'load-path (expand-file-name "~/gisemacs/.emacs.d/"))
+(add-to-list 'load-path (expand-file-name "~/gisemacs/cedet-1.0pre7/"))
 (add-to-list 'load-path (expand-file-name "~/gisemacs/ecb/"))
 (add-to-list 'load-path (expand-file-name "~/gisemacs/tree/"))
+
 
 
 
@@ -101,9 +103,9 @@
 
 
 
-(require 'ecb)
-;(require 'ecb-autoloads)
-(setq ecb-tip-of-the-day nil)
+
+
+
 
 
 
@@ -117,27 +119,6 @@
   )
 
 
-
-
-
-(defun ecb-expand-methods-nodes2 (&optional force-all)
-  (interactive "P")
-  (let* ((first-node (save-excursion
-                       (goto-char (point-min))
-                       (tree-buffer-get-node-at-point))))
-    (when ecb-expand-methods-switch-off-auto-expand
-      (ecb-toggle-auto-expand-tag-tree -1))
-    (ecb-expand-methods-node-internal (with-current-buffer ecb-methods-buffer-name
-                                         (tree-buffer-get-root))
-                                       10 force-all t t)))
-
-
-;; ECB custom settings
-(global-set-key (kbd "\C-c m") 'ecb-goto-window-methods)
-(global-set-key (kbd "\C-c e") 'ecb-expand-methods-nodes2)
-
-(add-hook 'after-save-hook 'ecb-expand-methods-nodes2)
-(add-hook 'ecb-activate-hook 'ecb-expand-methods-nodes2)
 
 
 
@@ -311,28 +292,13 @@
                ("i18n" (filename . ".*i18n/.*"))
                ("logs" (filename . ".*logs/.*"))))))
 
-			  
-  (ecb-layout-define "markolayout" right nil                                                                                          
-   (if (fboundp (quote ecb-set-methods-buffer)) (ecb-set-methods-buffer) (ecb-set-default-ecb-buffer))                               
-   (dotimes (i 2) (other-window 1) (if (equal (selected-window) ecb-compile-window) (other-window 1)))                                
-   (dotimes (i 1) (other-window 1) (if (equal (selected-window) ecb-compile-window) (other-window 1)))                                
-   )   
+
+
 
  (set-default-font "-adobe-courier-medium-r-normal--18-180-75-75-m-110-iso8859-1")
 
 ;; (custom-set-variables
 
-;;  '(ecb-auto-activate t)
-;;  '(ecb-auto-expand-tag-tree (quote all))
-;;  '(ecb-auto-update-methods-after-save t)
-;;  '(ecb-expand-methods-switch-off-auto-expand nil)
-;;  '(ecb-layout-name "markolayout")
-;;  '(ecb-methods-nodes-expand-spec (quote all))
-;;  '(ecb-options-version "2.40")
-;;  '(ecb-tag-jump-sets-mark nil)
-;;  '(ecb-toggle-auto-expand-tag-tree 1)
-;;  '(ecb-windows-width 0.2))
-;; (custom-set-faces
 
 ;;  '(linum ((t (:inherit (shadow default) :background "color-236")))))
 ;; (put 'scroll-left 'disabled nil)
@@ -341,24 +307,13 @@
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
 
-
-(custom-set-variables
   ;; custom-set-variables was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(dired-listing-switches "-lha -h --group-directories-first")
- '(ecb-auto-activate t)
- '(ecb-fix-window-size t)
- '(ecb-auto-expand-tag-tree (quote all))
- '(ecb-auto-update-methods-after-save t)
- '(ecb-expand-methods-switch-off-auto-expand nil)
- '(ecb-layout-name "markolayout")
- '(ecb-methods-nodes-expand-spec (quote all))
- '(ecb-options-version "2.40")
- '(ecb-tag-jump-sets-mark nil)
- '(ecb-toggle-auto-expand-tag-tree 1)
- '(ecb-windows-width 0.2))
+
+
+ 
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
